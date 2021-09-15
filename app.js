cc8202dac39002eda75971347d56eb13ca7cd1ed
YUN <yunmyeonghun@gmail.com>
2021-09-15 11:35:48 +0900

    study 11

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for request
 app.listen(3000);
 
 app.get('/', (req, res) => {
   //   res.send('<p>home page</p>');
-  res.sendFile('./views/index.html', { root: __dirname });
+  //   res.sendFile('./views/index.html', { root: __dirname });
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
   // res.send('<p>about page</p>');
-  res.sendFile('./views/about.html', { root: __dirname });
+  //   res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about');
+});
+
+app.get('/create', (req, res) => {
+  res.render('create');
 });
 
 // redirects
@@ -23,5 +32,6 @@ app.get('/about-us', (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  //   res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404');
 });
